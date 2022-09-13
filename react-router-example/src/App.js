6a3e1e1cf0ce6b592cd16ea73b39f1d2046260cc
김용호 <ashore92@naver.com>
2022-09-13 09:40:07 +0900

    오류 수정

--- react-router-example/src/App.js
+++ react-router-example/src/App.js
@@ -1,13 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
 import Profile from './pages/Profile';
 import About from './pages/About';
 
-const App = () => {
+
+function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
